eb0b9221d6bc28b0d9b06e988952b27e5cd2ee99
Danny Burke <danny@steamhaus.co.uk>
2020-09-05 14:51:00 +0100

    add working userdata script for kubeadm nodes

---
+++ terraform/userdata.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+swapoff -a 
+sudo yum update -y && yum upgrade -y
+sudo yum install -y git 
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf 
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system 
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+sudo setenforce 0 
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config 
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes 
+sudo systemctl enable --now kubelet 
+systemctl daemon-reload 
+systemctl restart kubelet
+systemctl restart kubeadm 
+systemctl restart kubectl
