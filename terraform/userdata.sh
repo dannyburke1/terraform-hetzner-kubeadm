00e38e15e1ab944686306b9eb7034d0828371cdd
Danny Burke <danny@steamhaus.co.uk>
2020-09-05 15:14:30 +0100

    Install Docker

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -2,6 +2,34 @@
 swapoff -a 
 sudo yum update -y && yum upgrade -y
 sudo yum install -y git 
+
+# Install Docker
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+yum-config-manager --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y containerd.io-1.2.13 docker-ce-19.03.11 docker-ce-cli-19.03.11 --skip-broken
+
+mkdir /etc/docker
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Install Kubernetes
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf 
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -25,3 +53,9 @@ systemctl daemon-reload
 systemctl restart kubelet
 systemctl restart kubeadm 
 systemctl restart kubectl
+systemctl daemon-reload
+systemctl restart docker
+
+sudo systemctl enable --now docker
+
+rm -rf /home/*/.ssh/known_hosts
