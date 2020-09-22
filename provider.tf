e2d36e74d1e8035c7e12963c9aa47068d77da234
Danny Burke <danny@steamhaus.co.uk>
2020-09-22 18:27:17 +0100

    add hetzner provider with required source

--- provider.tf
+++ provider.tf
@@ -3,5 +3,11 @@ provider "hcloud" {
 }
 
 terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "1.21.0"
+    }
+  }
   required_version = "0.13.2"
 }
